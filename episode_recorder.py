34dc34ed82c977e21b3d80577f5b40fc2be63463
MWransky <michael.wransky@udacity.com>
2016-11-19 20:53:32 -0500

    built episode recorder to save samples of observations for training

---
+++ episode_recorder.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+class episode_recorder():
+    def __init__(self, buffer_size=1000):
+        self.buffer = []
+        self.buffer_size = buffer_size
+
+    def add(self, experience):
+        if len(self.buffer) + 1 >= self.buffer_size:
+            self.buffer[0:(1+len(self.buffer))-self.buffer_size] = []
+        self.buffer.append(experience)
+
+    def sample(self, batch_size, trace_length):
+        sampled_episodes = random.sample(self.buffer, batch_size)
+        sampledTraces = []
+        for episode in sampled_episodes:
+            point = np.random.randint(0, len(episode)-trace_length)
+            sampledTraces.append(episode[point:point+trace_length])
+        sampledTraces = np.array(sampledTraces)
+        return np.reshape(sampledTraces, [batch_size*trace_length, 5])
